2b31a2b9578e680e42bfcfcda10bb665498531ce
Daniil <daniil.shishaev@gmail.com>
2025-03-20 14:38:46 +0300

    instance update

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -1,12 +1,12 @@
 import axios from "axios";
-const token = 'c453cd94-112f-4d72-aac7-9ce61bbe285f'
-const apiKey = '8c881c3f-ac4e-4bb3-ad5a-56edec988ece'
 
-export const instance=axios.create({
-    baseURL:"https://social-network.samuraijs.com/api/1.0/",
+
+export const instance= axios.create({
+    baseURL:process.env.REACT_APP_BASE_URL,
 
     headers:{
-        Authorization:`Bearer ${token}`,
-        // "API_KEY":apiKey
+        Authorization:`Bearer ${process.env.REACT_APP_AUTH_TOKEN}`,
+        // withCredentials:true,
+        "API-KEY":process.env.REACT_APP_API_KEY,
     }
 })
